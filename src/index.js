ccbe3ed0bbf177c3586c26d1384cee5d5aa6a16f
Angelika Friis <angelikafriis@gmail.com>
2024-11-24 00:02:09 +0100

    connected to database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
+
+connectDB()
 
 app.listen(PORT, () => {
-    console.log('server listening on port: ${PORT}')
+    console.log(`server listening on port: ${PORT}`)
 })
