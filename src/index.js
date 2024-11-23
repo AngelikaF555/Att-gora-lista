c062eeb816430a5410ff44ff0c4ac28546fdb003
Angelika Friis <angelikafriis@gmail.com>
2024-11-23 17:32:32 +0100

    added .env file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
-const PORT = 5001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log('server listening on port: ${PORT}')
