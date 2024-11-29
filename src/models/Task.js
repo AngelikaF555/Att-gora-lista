a6c3528ca05d5911a980778b991abb54a8516d82
Angelika Friis <angelikafriis@gmail.com>
2024-11-29 21:27:48 +0100

    Added task schema

---
+++ src/models/Task.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const TaskSchema = new mongoose.Schema({
+    title: { type: String, required: true},
+    status: { type: String, default: 'not done'},
+    estimatedTime: { type: String },
+    category: { type: String },
+    priority: { type: String }
+}, { timestamps: true})
+
+module.exports = mongoose.model('Task', TaskSchema)
+
